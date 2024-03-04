6df2c4e9a6d56599d1fa154495d169bbb6c0e373
azkali <a.ffcc7@gmail.com>
2024-03-04 20:43:02 +0100

    q2q: Unclutter and update fingerprint

--- lineage_q2q.mk
+++ lineage_q2q.mk
@@ -4,35 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/q2q/device.mk)
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from the device configuration.
+$(call inherit-product, device/samsung/q2q/device.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit some common Lineage stuff
+# Inherit from the Lineage configuration.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2208
-TARGET_SCREEN_WIDTH := 1768
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_q2q
 PRODUCT_DEVICE := q2q
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-F926B
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-BUILD_FINGERPRINT := "samsung/q2qxeea/q2q:13/TP1A.220624.014/F926BXXS3EWD9:user/release-keys"
-PRIVATE_BUILD_DESC := "q2qxxx-user 13 TP1A.220624.014 F926BXXS3EWD9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="q2qxxx-user 13 TP1A.220624.014 F926BXXS5FWK2 release-keys"
+BUILD_FINGERPRINT := "samsung/q2qxeea/q2q:13/TP1A.220624.014/F926BXXS5FWK2:user/release-keys"
