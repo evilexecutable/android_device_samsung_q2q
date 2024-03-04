ec4e3f19c6f8d49d47f6ba6bccbefbf37f2adbd1
azkali <a.ffcc7@gmail.com>
2024-03-04 20:42:25 +0100

    q2q: Make use of device.mk

--- device.mk
+++ device.mk
@@ -3,7 +3,28 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/q2q
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2208
+TARGET_SCREEN_WIDTH := 1768
+
+# Init
+PRODUCT_PACKAGES += \
+    init.q2q.rc
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Overlays
+#PRODUCT_PACKAGES += \
+#    FrameworksResq2q
 
 # call the common setup
 $(call inherit-product, device/samsung/sm8350-common/common.mk)
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
