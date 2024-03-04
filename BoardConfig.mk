d4557f94382790d80860adc72a5a3fbae65c2d17
azkali <a.ffcc7@gmail.com>
2024-03-04 20:41:09 +0100

    q2q: Improve and fix device properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,19 +3,37 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/q2q
-
+# Include the common OEM chipset BoardConfig.
 include device/samsung/sm8350-common/BoardConfigCommon.mk
 
-# Kernel
-TARGET_KERNEL_CONFIG		:= vendor/lineage_q2q_eur_openx_defconfig
-BOARD_NAME                  := SM-F926B
+DEVICE_PATH := device/samsung/q2q
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := q2q
 
 # Display
-TARGET_SCREEN_DENSITY := 372
+TARGET_SCREEN_DENSITY := 420
 
-# OTA assert
-TARGET_OTA_ASSERT_DEVICE := q2q
+# Kernel
+TARGET_KERNEL_CONFIG	    := vendor/lineage_q2q_eur_openx_defconfig
+BOARD_NAME                  := SM-F926B
+
+# Kernel Modules
+#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+#BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
+#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_boot
+#BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+#BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+#BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery $(DEVICE_PATH)/modules.include.vendor_ramdisk))
+#BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+#RECOVERY_KERNEL_MODULES := $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
+
+# Recovery
+TARGET_RECOVERY_DEFAULT_ROTATION := ROTATION_LEFT
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
